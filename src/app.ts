78b6a7e6c700a58e9acb4df2607fcd67a7b7be85
ElderGr <gbielrs@gmail.com>
2023-06-23 20:58:33 -0300

    refactor: add typescript in dev depencies and change entrypoint files

---
+++ src/app.ts
@@ -0,0 +1,40 @@
+// require('dotenv').config();
+
+import express, { Express } from 'express';
+// import routes from './routes';
+import cors from 'cors';
+import path from 'path'
+
+const app = express();
+// const server = require('http').Server(app);
+// const io = require('socket.io')(server);
+
+const connectedUsers = {};
+
+// io.on('connection', socket =>{
+
+//     const { user } = socket.handshake.query;
+    
+//     connectedUsers[user] = socket.id;
+// });
+app.use(express.json())
+app.use(cors());
+
+// app.use((req, res, next)=>{
+//     req.io = io;
+//     req.connectedUsers = connectedUsers;
+
+//     return next();
+// });
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+export function init(): Promise<Express>{
+
+    return Promise.resolve(app);
+}
+
+
+export default app;
+
+
