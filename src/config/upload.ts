2ee69749b3a634d4e24a008ff84238e86e5ac0a2
ElderGr <gbielrs@gmail.com>
2023-06-24 10:39:24 -0300

    fix: adjust in multer storage config

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,7 +2,7 @@ import multer from 'multer'
 import path from 'path'
 
 export default {
-  storage: new multer.diskStorage({
+  storage: multer.diskStorage({
     destination: path.resolve(__dirname, '..', '..', 'uploads'),
     filename: function(req, file, cb) {
       cb(null, file.originalname);
