41c441b8bee11d29abf6876d227bc96e40f7556a
ElderGr <gbielrs@gmail.com>
2023-06-30 23:59:38 -0300

    fix: husky test

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,6 @@ const port = process.env.PORT || 5000
 
 init().then(() => {
     app.listen(port, () => {
-        console.log(`Server up in ${port} port.`)
+        console.log(`Server up in ${port} port!!!.`)
     })
 })
