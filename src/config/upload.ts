dfe6025f9b47f525b369bc0c295083ca0c4b1a0c
ElderGr <gbielrs@gmail.com>
2023-09-14 20:55:59 -0300

    fix: adjust in file name store

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,11 +1,11 @@
-import multer from 'multer'
-import path from 'path'
+import multer from 'multer';
+import path from 'path';
 
 export default {
   storage: multer.diskStorage({
     destination: path.resolve(__dirname, '..', '..', 'uploads'),
-    filename: function(req, file, cb) {
-      cb(null, file.originalname);
-    }
-  })
+    filename: function (req, file, cb) {
+      cb(null, `${new Date().getTime()}_${file.originalname}`);
+    },
+  }),
 };
